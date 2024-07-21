f15096011ff66a17596b5da1c96f71a798471733
Finn Poduska <111396997+ImThatOneGuyInTheCorner@users.noreply.github.com>
2024-07-20 22:54:30 -0700

    Final Styling

--- script.js
+++ script.js
@@ -1,30 +1,111 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let icon = document.getElementById(`icon`)
+    icon.style.backgroundImage = "url('icon/Placeholder.png')"
     let button = document.getElementById("button")
     button.addEventListener(`click`, questLoad);
+
+    backAnimLoad()
 });
+
+function backAnimLoad() {
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    console.log(Math.floor(vw * 1.5))
+    let id = null;
+    const elem = document.getElementById("back");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 16);
+    function frame() {
+        if (pos == Math.floor(-vw * 1.5)) {
+            clearInterval(id);
+            backAnimLoad()
+            elem.style.bottom = "0px";
+        } else {
+            pos--;
+            elem.style.bottom = pos + "px";
+        }
+    }
+}
 
 function questLoad() {
     let questBox = document.getElementById(`quest`)
-    let questList = ["Can only enter through fire exit",
-        "Kill a player, if accused you must kys",
-        "Kill something (players included)",
-        "No 2-handed items",
-        "Can't use computer",
-        "No bought items",
-        "Cant use tp, must kys if someone tps you",
-        "Has to use inverse",
-        "Buddy system",
-        "Stalk a player from a distance",
-        "Mute (No talking)",
-        "Kill a monster",
-        "Cant open doors inside the facility",
-        "Cant leave the ship",
-        "Cant crouch around dogs (only dodging)",
-        "Boy who cried wolf (shout random monster names through out)",
+    let questList = ["You can only enter the building through fire exits",
+        "Kill a player by the end of the day, if accused or do not succeed you may not leave on the ship/must KYS",
+        "Kill a monster by the end of the day or you may not leave on the ship",
+        "You may not carry any item that takes both hands to hold",
+        "You may not open the terminal at any point",
+        "No purchasing or using purchased items",
+        "You can not use any teleporter, if someone teleports you, you must KYS",
+        "You may only enter the building via inverse tp [If you don't have it then re-roll]",
+        "You can only enter/exit the facility if with another player",
+        "Stalk a player from a distance, try not to get noticed",
+        "Must mute your mic for the entirety of the day",
+        "You can not open doors inside the facility",
+        "You can not leave the ship",
+        "You can not crouch",
+        "Shout random monster names throughout the day to scare crewmates",
         "Must stay out of sight of other players in the building",
-        "Crouch only",
-        "Roleplay as bear grills"]
+        "You must only crouch the whole day [If with another player, you can get uppies and jump over small inclines and climb ladders]",
+        "Roleplay as Bear Grylls"]
+    let nameBox = document.getElementById(`name`)
+    let nameList = ["The Fireman",
+        "The Murderer",
+        "The Poacher",
+        "The Weak",
+        "The Illiterate",
+        "The Cheapskate",
+        "The Old-Fashioned",
+        "The Daredevil",
+        "The Bestie",
+        "The Stalker",
+        "The Mute",
+        "The Confused",
+        "The Frightened",
+        "Mr. No-Knees",
+        "Boy Who Cried Wolf",
+        "The Unseen",
+        "The Cripple",
+        "Bear Grylls"]
+    let icon = document.getElementById(`icon`)
+    let iconList = ["url('icon/TFi.png')",
+        "url('icon/TMu.png')",
+        "url('icon/TPo.png')",
+        "url('icon/TWe.png')",
+        "url('icon/TIl.png')",
+        "url('icon/TCe.png')",
+        "url('icon/TOl.png')",
+        "url('icon/TDa.png')",
+        "url('icon/TBe.png')",
+        "url('icon/TSt.png')",
+        "url('icon/TMute.png')",
+        "url('icon/TCo.png')",
+        "url('icon/TFr.png')",
+        "url('icon/MNo.png')",
+        "url('icon/BWh.png')",
+        "url('icon/TUn.png')",
+        "url('icon/TCr.png')",
+        "url('icon/BGr.png')"]
 
     // number of entries
-    questBox.innerText = questList[Math.floor(Math.random() * 19)]
+    let randNum = Math.floor(Math.random() * 18)
+    questBox.innerText = questList[randNum]
+    nameBox.innerText = nameList[randNum]
+    icon.style.backgroundImage = iconList[randNum]
+
+    icon.style.opacity = '0%';
+    let classCont = document.getElementById("classCont")
+    classCont.style.opacity = '0%';
+    let id = null;
+    let opa = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (opa == 100) {
+            clearInterval(id);
+        } else {
+            opa++;
+            classCont.style.opacity = opa + "%";
+            icon.style.opacity = opa + "%";
+        }
+    }
 }
